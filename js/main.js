0898c0a18e14fdca685d753411c6dbadb5e719a2
sam3213 <112513734+sam3213@users.noreply.github.com>
2023-07-25 16:51:49 -0500

    trabajo 1 felipe

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+
+function SUMA(){
+
+    let A = 0;
+    let B = 0;
+    let SUMA = 0;
+
+    A = parseInt(prompt("por favor ingrese el primer valor")
+    B = parseInt(prompt("por favor ingrese el segundo valor")
+
+    SUMA = A + B;
+
+    alert("La suma es igual a: " + SUMA);
+}
